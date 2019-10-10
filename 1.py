6aec907c56d4a74ea2c7479f0af2298ae1a30219
Vapekreng <vapekreng@gmail.com>
2019-10-10 16:04:36 +0200

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,4 +1,4 @@
-print('Вводим с клавиатуры 2 целых числа и выводим на экран их сумму')
+print('Сложение 2 чисел')
 number_one = int(input('введите первое число: '))
 number_two = int(input('введите второе число: '))
 sum_of_numbers = number_one + number_two
